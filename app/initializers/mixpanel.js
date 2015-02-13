cd2ad87c65fbba3d4240eff6e0e31b47568f7802
Christopher Troup <ctroup@freshbooks.com>
2015-02-13 10:05:28 -0500

    Add mixpanel helper to ember-data adapters

--- app/initializers/mixpanel.js
+++ app/initializers/mixpanel.js
@@ -2,9 +2,11 @@ import Ember from 'ember';
 import Views from 'ember-mixpanel/views';
 import Controllers from 'ember-mixpanel/controllers';
 import Routes from 'ember-mixpanel/routes';
+import Adapters from 'ember-mixpanel/adapters';
 
 export default {
   name: 'mixpanel',
+  before:'store',
 
   initialize: function(container, application) {
     Ember.View.reopen({
@@ -18,8 +20,14 @@ export default {
     application.register('mixpanel:views', Views, { instantiate: true });
     application.register('mixpanel:controllers', Controllers, { instantiate: true });
     application.register('mixpanel:routes', Routes, { instantiate: true });
+    container.register('mixpanel:adapters', Adapters, { instantiate: true });
+
     application.inject('view', 'mixpanel', 'mixpanel:views');
     application.inject('controller', 'mixpanel', 'mixpanel:controllers');
     application.inject('route', 'mixpanel', 'mixpanel:routes');
+
+    application.inject('dataAdapter', 'mixpanel', 'mixpanel:adapters');
+    application.inject('store', 'mixpanel', 'mixpanel:adapters');
+    application.inject('adapter', 'mixpanel', 'mixpanel:adapters');
   }
 };
